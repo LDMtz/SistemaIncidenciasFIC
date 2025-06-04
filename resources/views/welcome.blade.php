1c0a3622598a63fe6324daea894b9b7b1af012f8
LDMtz <leonciodanielmartinez@gmail.com>
2025-06-03 21:04:21 -0700

    Inicializacion de laravel

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Sistema de incidencias - Facultad de Informática Culiacán</title>
+        @vite('resources/css/app.css') <!-- TailwindCss-->
+    </head>
+    <body class="bg-gray-900">
+        <h1 class="text-white">Hello world!a</h1>
+    </body>
+</html>
